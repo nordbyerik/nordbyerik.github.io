45b778f8d330b4923aa1b355d6312f5d2c48cdf3
nordbyerik <nordby.erik3@gmail.com>
2024-05-21 19:57:44 -0400

    Updated

--- erik-website/src/components/LogoSection.vue
+++ erik-website/src/components/LogoSection.vue
@@ -1,23 +1,30 @@
 <template>
   <div class="social-icons" id="LogoSection">
-    <a href="https://github.com/yourusername" target="_blank">
+    <a href="https://github.com/nordbyerik" target="_blank">
       <img src="@/assets/github.png" alt="GitHub" />
     </a>
-    <a href="https://www.notion.so/yourpage" target="_blank">
+    <a
+      href="https://safe-mustang-98d.notion.site/Erik-s-Notes-781314fa142647b7b539c56af65a5c6e?pvs=4"
+      target="_blank"
+    >
       <img src="@/assets/notion.webp" alt="Notion" />
     </a>
-    <a href="https://www.linkedin.com/in/yourusername" target="_blank">
+    <a href="https://www.linkedin.com/in/erik-nordby/" target="_blank">
       <img src="@/assets/linkedin.webp" alt="LinkedIn" />
+    </a>
+    <a href="https://linktr.ee/eriknordby" target="_blank">
+      <img src="@/assets/linktree.png" alt="Twitter" />
     </a>
   </div>
 </template>
-
 <style>
 .social-icons a img {
   width: 40px; /* Adjust this value as needed */
   height: 40px; /* Adjust this value to maintain the aspect ratio or as desired */
   vertical-align: middle; /* Centers the icon vertically */
   margin-right: 10px; /* Adds spacing between icons, adjust as needed */
+  color: black;
+  decoration: none;
 }
 
 .social-icons {
